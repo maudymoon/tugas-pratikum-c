a72a36208347ca493bc41e533e52257711e90627
maudymoon <141110724+maudymoon@users.noreply.github.com>
2023-10-12 14:58:04 +0700

    Add files via upload

---
+++ latihan hari-2-cpp/Tugas Mandiri 1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    double alas = 8;
+    double tinggi = 5;
+    
+    double luas = 0.5 * alas * tinggi;
+    
+    printf("Luas segitiga dengan alas %.2f cm dan tinggi %.2f cm adalah %.2f cm^2\n", alas, tinggi, luas);
+    
+    return 0;
+}
