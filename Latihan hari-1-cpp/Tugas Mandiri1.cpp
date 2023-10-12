5cd69d9a6f20b384df3c7a14b4390df867ec07a7
maudymoon <141110724+maudymoon@users.noreply.github.com>
2023-10-12 14:56:03 +0700

    Add files via upload

---
+++ Latihan hari-1-cpp/Tugas Mandiri1.cpp
@@ -0,0 +1,15 @@
+# include "stdio.h"
+
+int main()
+{
+	char*nim = "2143009";
+	char*nama = "Maudy Shofiyah";
+	char*jurusan = "Teknik Informatika";
+	
+	printf("informasi Mahasiswa :\n");
+	printf("NIM = %s\n",nim);
+	printf("Nama = %s\n",nama);
+	printf("Jurusan = %s",jurusan);
+	
+	return 0;
+}
---
+++ Latihan hari-1-cpp/Tugas Mandiri2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int umur = 25;
+	int tinggi = 170;
+	
+	printf("umur saya %d tahun.\n", umur);
+	printf("tinggi saya %d cm", tinggi);
+	
+	return 0;
+}
